b9c8bc9d029eb82b9e46f60ad7474a369e7f37a9
caidle <67764059+caidle@users.noreply.github.com>
2024-01-08 22:58:05 -0500

    Changed API key

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "3abd40f778e68af1d547coc6ct4952fd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
